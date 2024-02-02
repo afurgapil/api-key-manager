59ce3a3972a0b63b14491852fb112fbab183f255
Gapil <gapilsoft@gmail.com>
2024-02-02 15:58:15 +0300

    update

--- mailOptions/resetMail.js
+++ mailOptions/resetMail.js
@@ -1,5 +1,5 @@
 module.exports = function resetMail(email, encodedEmail, code) {
-  const resetCheckURL = "http://localhost:3001/auth/reset/check";
+  const resetCheckURL = "http://localhost:3001/reset/check";
 
   const mailOptions = {
     from: "apicookbook@gmail.com",
@@ -7,7 +7,7 @@ module.exports = function resetMail(email, encodedEmail, code) {
     subject: "Password Reset Request",
     html: `
       <p>Hello,</p>
-      <p>To reset your password, click <a href="${resetCheckURL}?mail=${encodedEmail}">here</a> or use the following code:</p>
+      <p>To reset your password, click <a href="${resetCheckURL}/${encodedEmail}">here</a> or use the following code:</p>
       <p style="display: inline-block; padding: 10px 20px; background-color: #007BFF; color: #fff; text-decoration: none; border-radius: 5px;">${code}</p>
       <p>If you did not request a password reset, you can ignore this email.</p>
       <p>Best regards.</p>
